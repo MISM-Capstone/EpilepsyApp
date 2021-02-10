dbdef04998be924b5a4ec613e297eed171eab165
iwwhite <isaacww@byu.edu>
2021-02-10 07:41:07 -0700

    The database connection now works!

--- src/pages/Home/SurveyHistory.tsx
+++ src/pages/Home/SurveyHistory.tsx
@@ -16,10 +16,10 @@ type RenderProps = {
 function RenderItem(props:RenderProps) {
     return (
         <View>
-            <Text>ID: log.seizure_id</Text>
-            <Text>Date: log.date</Text>
-            <Text>Location: log.location</Text>
-            <Text>Notes: log.notes</Text>
+            <Text>ID: {props.log.seizure_id}</Text>
+            <Text>Date: {props.log.date}</Text>
+            <Text>Location: {props.log.location}</Text>
+            <Text>Notes: {props.log.notes}</Text>
         </View>
     )
 }
@@ -27,12 +27,17 @@ function RenderItem(props:RenderProps) {
 export default function SurveyHistory(props:Props) {
     const [records, setRecords] = useState<any[]>([]);
 
+    // Similar to class based componentDidMount This will run
+    // when the component first renders.
     useEffect(() => {
         (async () => {
             const results = await SeizureLogDao.getLogs();
             setRecords(results);
         })();
-    },[]);
+    },[]); // These square brackets define when the effect should
+           // run again. If they are empty, it will only run when
+           // the component renders. If you pass a state variable
+           // (i.e. records) it will run each time records changes.
 
 
     return (
@@ -40,7 +45,7 @@ export default function SurveyHistory(props:Props) {
             <FlatList 
                 data={records}
                 renderItem={({item}) => <RenderItem log={item} />}
-                keyExtractor={(item) => item.id}
+                keyExtractor={(item) => item.seizure_id.toString()}
             />
         </SafeAreaView >
     )
