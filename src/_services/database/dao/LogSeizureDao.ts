5fbeca46ef5fb6ebe671bc06f6c0469be2b8962c
ross <rosenlof.ross@gmail.com>
2021-02-15 12:46:23 -0700

    changing date formats on insert queries

--- src/_services/database/dao/LogSeizureDao.ts
+++ src/_services/database/dao/LogSeizureDao.ts
@@ -2,9 +2,10 @@ import Dao from "./Dao";
 
 export default class LogSeizureDao extends Dao {
     static async insertSeizure(date: Date, location: string, notes: string) {
+        let date_string = date.toJSON().substring(0,10);
         const sql = `
             INSERT INTO seizure_log (date, location, notes) 
-            VALUES ('${date}','${location}','${notes}');
+            VALUES ('${date_string}','${location}','${notes}');
         `;
         const db = await this.getDatabase();
         const results = await db.executeSql(sql);
