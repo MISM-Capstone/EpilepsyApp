17b0f930ca14d2a28a3b769fe518c21a1009a244
ross <rosenlof.ross@gmail.com>
2021-02-16 10:38:53 -0700

    adding chart setup

--- src/pages/Trends/Chart.tsx
+++ src/pages/Trends/Chart.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import {
-    Text,
-    View} from 'react-native';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-
+import { VictoryLine, VictoryChart, VictoryTheme } from "victory-native";
 
 const Chart = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Chart</Text>
-        </View>
+            <VictoryChart width={350} theme={VictoryTheme.material}>
+                <VictoryLine
+                    style={{
+                        data: { stroke: "#c43a31" },
+                        parent: { border: "1px solid #ccc" }
+                    }}
+                    data={[
+                        { x: 1, y: 2 },
+                        { x: 2, y: 3 },
+                        { x: 3, y: 5 },
+                        { x: 4, y: 4 },
+                        { x: 5, y: 7 }
+                    ]}
+                    animate={{
+                        duration: 2000,
+                        onLoad: { duration: 1000 }
+                      }}
+                />
+            </VictoryChart>
+        </SafeAreaView>
     );
 }
 
