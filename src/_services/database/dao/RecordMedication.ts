df2e099a5d322fa35bf313073b1c5c2919fc4718
ross <rosenlof.ross@gmail.com>
2021-03-08 09:15:54 -0700

    record medication survey

---
+++ src/_services/database/dao/RecordMedication.ts
@@ -0,0 +1,26 @@
+import { ResultSet } from "react-native-sqlite-storage";
+import Dao from "./Dao";
+
+export default class RecordMedicationDao extends Dao {
+    static async insertMedication(date: Date, time:Date, medication: string, dosage: string, notes: string) {
+        let date_string = date.toJSON().substring(0,10);
+        let time_string = time.toLocaleTimeString(); 
+        console.log('TIME STRING: ', time_string)
+        const sql = `
+            INSERT INTO medication_log (date, time, medication, dosage, notes) 
+            VALUES (?,?,?,?,?);
+        `;
+        const params = [date_string, time_string, medication, dosage, notes];
+        const db = await this.getDatabase();
+        let results:ResultSet[] = [];
+        await db.transaction(async tx => {
+            let [, result] = await tx.executeSql(sql,params);
+            results.push(result);
+        });
+        return results;
+    }
+
+    static async get() {
+        
+    }
+}
--- src/_services/database/queries/create-tables.sql
+++ src/_services/database/queries/create-tables.sql
@@ -5,3 +5,12 @@ CREATE TABLE seizure_log (
     location TEXT NOT NULL,
     notes TEXT NOT NULL
 );
+
+CREATE TABLE medication_log (
+    medication_id INTEGER PRIMARY KEY,
+    date DATE NOT NULL,
+    time TEXT NOT NULL,
+    medication TEXT NOT NULL,
+    dosage TEXT NOT NULL,
+    notes TEXT NOT NULL
+);
