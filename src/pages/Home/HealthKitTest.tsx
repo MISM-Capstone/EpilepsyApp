ffd0a24c66ea4274deac22be32764552c1e69e60
ross <rosenlof.ross@gmail.com>
2021-03-15 12:09:29 -0600

    adding more healthkit data for testing

--- src/pages/Home/HealthKitTest.tsx
+++ src/pages/Home/HealthKitTest.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
-import AppleHealthKit, { HealthValue, HealthKitPermissions, HealthInputOptions } from 'react-native-health';
+import AppleHealthKit, { HealthValue, HealthKitPermissions, HealthInputOptions, HealthUnit } from 'react-native-health';
 
 type Props = {
     navigation: any;
@@ -8,37 +8,83 @@ type Props = {
 
 const permissions = {
     permissions: {
-      read: [
-        AppleHealthKit.Constants.Permissions.SleepAnalysis,
-        AppleHealthKit.Constants.Permissions.ActiveEnergyBurned,
-        AppleHealthKit.Constants.Permissions.Weight
-      ],
-      write: [
-        AppleHealthKit.Constants.Permissions.Steps,
-      ],
+        read: [
+            AppleHealthKit.Constants.Permissions.SleepAnalysis,
+            AppleHealthKit.Constants.Permissions.ActiveEnergyBurned,
+            AppleHealthKit.Constants.Permissions.Weight
+        ],
+        write: [
+            AppleHealthKit.Constants.Permissions.Steps,
+        ],
     }
-  } as HealthKitPermissions
-  
-  
-  AppleHealthKit.initHealthKit(permissions, (error: string) => {  
+} as HealthKitPermissions
+
+AppleHealthKit.initHealthKit(permissions, (error: string) => {
     if (error) {
-     console.log('[ERROR] Cannot grant permissions!')
+        console.log('[ERROR] Cannot grant permissions!')
     }
-  
-    const options: HealthInputOptions = {
-      startDate: (new Date(2020, 1, 1)).toISOString(),
-    }
-  
-    AppleHealthKit.getSleepSamples(undefined, (callbackError: string, results: HealthValue[]) => {
-      /* Samples are now collected from HealthKit */
-      console.log('SLEEP SAMPLES: ', results);
-    });
-  });
+});
 
 export default function HealthKitTest(props: Props) {
+    const options: any = { startDate: (new Date(2020, 1, 1)).toISOString() }
+    const [sleep, setSleep] = useState<HealthValue>();
+    const [sleepLoaded, setSleepLoaded] = useState<boolean>(false);
+    const [weight, setWeight] = useState<HealthValue>();
+    const [weightLoaded, setWeightLoaded] = useState<boolean>(false);
+    const [energyBurned, setEnergyBurned] = useState<HealthValue[]>();
+    const [energyLoaded, setEnergyLoaded] = useState<boolean>(false);
+
+    // TODO: add undefined values if there is no data entered.
+    useEffect(() => {
+        AppleHealthKit.getSleepSamples(options, (err: string, results: HealthValue) => {
+            try {
+                console.log(results);
+                setSleep(results);
+                setSleepLoaded(true);
+            } catch {
+                console.error(err);
+            }
+        });
+        AppleHealthKit.getActiveEnergyBurned(options, (err: string, results: HealthValue[]) => {
+            try {
+                console.log(results);
+                setEnergyBurned(results);
+                setEnergyLoaded(true);
+            } catch {
+                console.error(err);
+            }
+        });
+        AppleHealthKit.getLatestWeight(options, (err: string, results: HealthValue) => {
+            try {
+                console.log(results);
+                setWeight(results);
+                setWeightLoaded(true);
+            } catch {
+                console.error(err);
+            }
+        });
+    }, []);
+
     return (
         <View>
-            <Text>Healthkit Test</Text>
+            {sleepLoaded &&
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>Sleep JSON data:</Text>
+                    <Text>{JSON.stringify(sleep).toString()}</Text>
+                </View>
+            }
+            {energyLoaded &&
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>Energy Burned JSON data:</Text>
+                    <Text>{JSON.stringify(energyBurned).toString()}</Text>
+                </View>
+            }
+            {weightLoaded &&
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>Weight JSON data:</Text>
+                    <Text>{JSON.stringify(weight).toString()}</Text>
+                </View>
+            }
         </View>
     )
 }
