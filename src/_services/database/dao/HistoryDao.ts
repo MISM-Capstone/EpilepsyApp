4f3b3a4b5fa7736e702e3cf4061de42786db9519
ross <rosenlof.ross@gmail.com>
2021-03-22 09:55:34 -0600

    adding delete, get by id, and update methods to dao

--- src/_services/database/dao/HistoryDao.ts
+++ src/_services/database/dao/HistoryDao.ts
@@ -1,3 +1,4 @@
+import { ResultSet } from "react-native-sqlite-storage";
 import Dao from "./Dao";
 // import SQLite from "react-native-sqlite-storage";
 
@@ -30,6 +31,20 @@ export default class HistoryDao extends Dao {
         return this.SetResultsToList(results[0].rows);
     }
 
+    static async getSeizureLogById(seizure_id: number | string) {
+        const sql = `
+            SELECT
+                *
+            FROM
+                seizure_log
+            WHERE 
+                seizure_id = ?
+        ;`;
+        const db = await this.getDatabase();
+        const results = await db.executeSql(sql, [seizure_id]);
+        return this.SetResultsToList(results[0].rows);
+    }
+
     static async deleteSeizureLog(id: number | string) {
         const sql = `
             DELETE FROM seizure_log
@@ -38,6 +53,28 @@ export default class HistoryDao extends Dao {
         const db = await this.getDatabase();
         const results = await db.executeSql(sql, [id]);
         return this.SetResultsToList(results[0].rows);
+    }
+
+    static async updateSeizureLog(id: number | string, date: Date, time: Date, location: string, notes: string) {
+        let date_string = date.toJSON().substring(0, 10);
+        let time_string = time.toLocaleTimeString();
+        const sql = `
+            UPDATE seizure_log 
+            SET date = ?, 
+                time = ?,
+                location = ?,
+                notes = ? 
+            WHERE
+                seizure_id = ?
+        `;
+        const params = [date_string, time_string, location, notes, id];
+        const db = await this.getDatabase();
+        let results: ResultSet[] = [];
+        await db.transaction(async tx => {
+            let [, result] = await tx.executeSql(sql, params);
+            results.push(result);
+        });
+        return results;
     }
 
     // SURVEY LOGS
@@ -67,6 +104,20 @@ export default class HistoryDao extends Dao {
         return this.SetResultsToList(results[0].rows);
     }
 
+    static async getSurveyLogById(survey_id: number | string) {
+        const sql = `
+            SELECT
+                *
+            FROM
+                survey_log
+            WHERE 
+                survey_id = ?
+        ;`;
+        const db = await this.getDatabase();
+        const results = await db.executeSql(sql, [survey_id]);
+        return this.SetResultsToList(results[0].rows);
+    }
+
     static async deleteSurveyLog(id: number | string) {
         const sql = `
             DELETE FROM survey_log
@@ -75,6 +126,40 @@ export default class HistoryDao extends Dao {
         const db = await this.getDatabase();
         const results = await db.executeSql(sql, [id]);
         return this.SetResultsToList(results[0].rows);
+    }
+
+    static async updateSurveyLog(id: number | string, date: Date, sleep_start_date: Date, sleep_end_date: Date, stress_level: string, illness: boolean, fever: boolean, miss_meal: boolean, medication: boolean) {
+        let date_string = date.toJSON().substring(0, 10);
+        const sql = `
+            UPDATE survey_log 
+            SET    
+                date = ?, 
+                sleep_start_date =?,
+                sleep_end_date = ?, 
+                stress_level = ?,
+                illness = ?,
+                fever = ?,
+                miss_meal = ?,
+                medication = ? 
+            WHERE 
+                survey_id = ?
+            ;
+        `;
+        const params = [
+            date_string,
+            sleep_start_date.toString(),
+            sleep_end_date.toString(),
+            stress_level,
+            illness.toString(),
+            fever.toString(),
+            miss_meal.toString(),
+            medication.toString(),
+            id
+        ]
+        console.log('params: ', params);
+        const db = await this.getDatabase();
+        const results = await db.executeSql(sql, params);
+        return results;
     }
 
     // MEDICATION LOGS
@@ -104,6 +189,20 @@ export default class HistoryDao extends Dao {
         return this.SetResultsToList(results[0].rows);
     }
 
+    static async getMedicationLogById(medication_id: number | string) {
+        const sql = `
+            SELECT
+                *
+            FROM
+                medication_log
+            WHERE 
+                medication_id = ?
+        ;`;
+        const db = await this.getDatabase();
+        const results = await db.executeSql(sql, [medication_id]);
+        return this.SetResultsToList(results[0].rows);
+    }
+
     static async deleteMedicationLog(id: number | string) {
         const sql = `
             DELETE FROM medication_log
@@ -112,6 +211,31 @@ export default class HistoryDao extends Dao {
         const db = await this.getDatabase();
         const results = await db.executeSql(sql, [id]);
         return this.SetResultsToList(results[0].rows);
+    }
+
+    static async updateMedication(id: number | string, date: Date, time:Date, medication: string, dosage: string, notes: string) {
+        let date_string = date.toJSON().substring(0,10);
+        let time_string = time.toLocaleTimeString(); 
+        console.log('TIME STRING: ', time_string)
+        const sql = `
+            UPDATE medication_log 
+            SET
+                date = ?, 
+                time = ?,
+                medication = ?, 
+                dosage = ?, 
+                notes = ?
+            WHERE
+                medication_id = ?
+        ;`;
+        const params = [date_string, time_string, medication, dosage, notes, id];
+        const db = await this.getDatabase();
+        let results:ResultSet[] = [];
+        await db.transaction(async tx => {
+            let [, result] = await tx.executeSql(sql,params);
+            results.push(result);
+        });
+        return results;
     }
 
     // COMBINED LOGS
