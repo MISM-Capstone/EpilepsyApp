ddcbfe32d395905f785b72de8bcd93bfebda4697
ross <rosenlof.ross@gmail.com>
2021-03-22 09:55:48 -0600

    starting update data pages

--- src/_services/helpers/sleepDates.service.ts
+++ src/_services/helpers/sleepDates.service.ts
@@ -7,15 +7,10 @@ type returnProps = {
 
 // Default structure for getting chart data
 const getSleepTime = (start: Date, end: Date): returnProps => {
-    console.log(start);
-    console.log(end);
-
     const total: number = (new Date(end).getTime() - new Date(start).getTime()) / 1000 / 60 /60;
     const hours: number = Math.floor(total);
     const minutes: number = Math.floor((total - hours) * 60);
     
-    console.log(total - hours);
-
     return {hours, minutes};
 }
 
