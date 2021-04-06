d51dcb48d757ef0167aa294bbe4ff55c4d23b969
ross <rosenlof.ross@gmail.com>
2021-04-05 21:44:07 -0600

    updating healthkit page

--- src/pages/Home/HealthKitTest.tsx
+++ src/pages/Home/HealthKitTest.tsx
@@ -6,11 +6,8 @@ const permissions = {
     permissions: {
         read: [
             AppleHealthKit.Constants.Permissions.SleepAnalysis,
-            AppleHealthKit.Constants.Permissions.ActiveEnergyBurned,
+            AppleHealthKit.Constants.Permissions.AppleExerciseTime,
             AppleHealthKit.Constants.Permissions.Weight
-        ],
-        write: [
-            AppleHealthKit.Constants.Permissions.Steps,
         ],
     }
 } as HealthKitPermissions
@@ -22,13 +19,13 @@ AppleHealthKit.initHealthKit(permissions, (error: string) => {
 });
 
 export default function HealthKitTest() {
-    const options: any = { startDate: (new Date(2020, 1, 1)).toISOString() }
-    const [sleep, setSleep] = useState<HealthValue>();
+    const options: any = { startDate: (new Date(2021, 1, 1)).toISOString() }
+    const [sleep, setSleep] = useState<HealthValue | any>();
     const [sleepLoaded, setSleepLoaded] = useState<boolean>(false);
-    const [weight, setWeight] = useState<HealthValue | string>();
+    const [weight, setWeight] = useState<HealthValue | string | any>();
     const [weightLoaded, setWeightLoaded] = useState<boolean>(false);
-    const [energyBurned, setEnergyBurned] = useState<HealthValue[]>();
-    const [energyLoaded, setEnergyLoaded] = useState<boolean>(false);
+    const [exercise, setExercise] = useState<HealthValue[] | any[]>();
+    const [exerciseLoaded, setExerciseLoaded] = useState<boolean>(false);
 
     // TODO: add undefined values if there is no data entered.
     useEffect(() => {
@@ -40,17 +37,17 @@ export default function HealthKitTest() {
                 console.error(err);
             }
         });
-        AppleHealthKit.getActiveEnergyBurned(options, (err: string, results: HealthValue[]) => {
+        AppleHealthKit.getAppleExerciseTime(options, (err: string, results: HealthValue[]) => {
             try {
-                setEnergyBurned(results);
-                setEnergyLoaded(true);
+                setExercise(results);
+                setExerciseLoaded(true);
             } catch {
                 console.error(err);
             }
         });
         AppleHealthKit.getLatestWeight(options, (err: string, results: HealthValue) => {
             try {
-                if (results !== undefined){
+                if (results !== undefined) {
 
                     setWeight(results);
                 } else {
@@ -64,23 +61,49 @@ export default function HealthKitTest() {
     }, []);
 
     return (
-        <View>
+        <View style={{ padding: 12 }}>
+            <Text style={{ fontStyle: 'italic', }}>This data comes from Apple HealthKit. While not currently used by our application, we plan to implement this data in future versions of the app.</Text>
+            <Text style={{ fontStyle: 'italic', }}>The range for this data is the past year.</Text>
             {sleepLoaded &&
                 <View>
-                    <Text style={{ fontWeight: 'bold' }}>Sleep JSON data:</Text>
-                    <Text>{JSON.stringify(sleep).toString()}</Text>
+                    <Text style={{ fontWeight: 'bold' }}>Sleep data:</Text>
+                    {sleep.length > 0 ?
+                        // <Text>{JSON.stringify(sleep)}</Text>
+                        sleep.map(function (sleepEvent: any) {
+                            return (
+                            <View style={{padding: 6, backgroundColor: 'lightgray', margin: 6}}>
+                                <Text>Start Date: {sleepEvent.startDate}</Text>
+                                <Text>End Date: {sleepEvent.startDate}</Text>
+                                <Text>Status: {sleepEvent.value}</Text>
+                            </View>
+                            )
+                        })
+                    :
+                        <Text>No sleep data recorded.</Text>
+                    }
+                    
                 </View>
             }
-            {energyLoaded &&
+            {exerciseLoaded &&
                 <View>
-                    <Text style={{ fontWeight: 'bold' }}>Energy Burned JSON data:</Text>
-                    <Text>{JSON.stringify(energyBurned).toString()}</Text>
+                    <Text style={{ fontWeight: 'bold' }}>Energy Burned data:</Text>
+                    {exercise!.length > 0 ?
+                        <Text>{JSON.stringify(exercise).toString()}</Text>
+                        :
+                        <Text>No exercise data recorded.</Text>
+                    }
+
                 </View>
             }
             {weightLoaded &&
                 <View>
-                    <Text style={{ fontWeight: 'bold' }}>Weight JSON data:</Text>
-                    <Text>{JSON.stringify(weight).toString()}</Text>
+                    <Text style={{ fontWeight: 'bold' }}>Weight data:</Text>
+                    {weight ?
+                        <Text>User's weight: {weight.value} lbs.</Text>
+                        :
+                        <Text>No user weight recorded.</Text>
+                    }
+
                 </View>
             }
         </View>
